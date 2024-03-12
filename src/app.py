7bb9ec8146c92cb5200d2a37b00169230d4f962c
kopcaboh <kopcaboh@fel.cvut.cz>
2024-03-12 17:29:15 +0100

    Snaha používat python

---
+++ src/app.py
@@ -0,0 +1,5 @@
+
+from browser import document, svg
+
+def draw_svg_canvas():
+    svg_canvas = svg.svg()
--- src/index.py
+++ src/index.py
@@ -1,12 +1,19 @@
-from browser import document, html
+from browser import document, html, window
 
 root = document["root"]
+screen_height = window.innerHeight
+screen_width = window.innerWidth
 
 def say_some_foolery():
+    svg_canvas = document.createElement("svg")
+    # svg_canvas = svg.svg(x=0, y=0,
+    #                      width=window.width, height=window.height,
+    #                      fill="blue")
     new_tag = document.createElement("h1")
-    new_text = document.createTextNode("Ahoj zlato")
+    new_text = document.createTextNode("Hello world")
     new_tag.appendChild(new_text)
 
     root.appendChild(new_tag)
+    root.appendChild(svg_canvas)
 
 say_some_foolery()
