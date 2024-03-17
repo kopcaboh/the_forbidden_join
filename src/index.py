90a508e82f0e4427c6f97ae9533d9b8655d1ac9b
kopcaboh <kopcaboh@fel.cvut.cz>
2024-03-17 18:48:34 +0100

    Added content container, change of index structure!

--- src/index.py
+++ src/index.py
@@ -2,15 +2,17 @@ from browser import document, html, window
 import memory_block.memory_block as memory_block
 import memory.memory as memory
 import table.table as table
-from utils.utils import highlight_row
+from utils.utils import highlight_row, connect_points
 
 root = document["root"]
+content = document["content-container"]
 screen_height = window.innerHeight
 screen_width = window.innerWidth
 
 def say_some_foolery():
     element = table.show()
 
-    root.appendChild(element)
+    content.appendChild(element)
+    
 
 say_some_foolery()
