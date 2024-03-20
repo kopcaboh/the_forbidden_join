db27be716a8e2a2823810dfcab33676ff64d18a5
kopcaboh <kopcaboh@fel.cvut.cz>
2024-03-20 14:29:16 +0100

    Highlight styles for memory blocks

--- src/index.py
+++ src/index.py
@@ -1,8 +1,8 @@
 from browser import document, html, window
-import memory_block.memory_block as memory_block
+from memory_block.memory_block import *
 import memory.memory as memory
 import table.table as table
-from utils.utils import highlight_row, connect_points, Point
+from utils.utils import *
 
 root = document["root"]
 content = document["content-container"]
@@ -10,9 +10,18 @@ screen_height = window.innerHeight
 screen_width = window.innerWidth
 
 def say_some_foolery():
-    element = table.show()
-
-    content.appendChild(element)
+    memblocks = [show() for i in range(10)]
     
+    for i in range(0, 3):
+        mark_used(memblocks[i])
+        
+    for i in range(3, 7):
+        mark_active(memblocks[i])
+
+
+    for memblock in memblocks:
+        content.appendChild(memblock)
+
+      
 
 say_some_foolery()
--- src/memory_block/memory_block.py
+++ src/memory_block/memory_block.py
@@ -5,14 +5,27 @@ from browser import document
 style = """
     width: 1.5vw;
     height: 1.5vw;
-    background-color: #a1dadd;
-    border: 2px solid #a1bcdd;
+    border-width: 2px;
+    border-style: solid;
     border-radius: 2px;
     margin: 5px;
 """
 
+def mark_normal(mem_block):
+    mem_block.style.backgroundColor = "#D7CB84"
+    mem_block.style.borderColor = "#CDBD65"
+
+def mark_active(mem_block):
+    mem_block.style.backgroundColor = "#E07A5F"
+    mem_block.style.borderColor = "#DB6443"
+
+def mark_used(mem_block):
+    mem_block.style.backgroundColor = "#928FB7"
+    mem_block.style.borderColor = "#7976A7"
+
 def show():
     element = document.createElement('div')
     element.style.cssText = style
+    mark_normal(element)
 
     return element
