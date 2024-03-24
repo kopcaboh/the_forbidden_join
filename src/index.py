6c332b6c9b9a6d97d3567cf16532d9d7d4798fa4
kopcaboh <kopcaboh@fel.cvut.cz>
2024-03-24 22:33:37 +0100

    Displaying intro ont index, basic index styling and fonts

--- src/index.py
+++ src/index.py
@@ -1,27 +1,44 @@
 from browser import document, html, window
-from memory_block.memory_block import *
+import memory_block.memory_block as memory_block
 import memory.memory as memory
 import table.table as table
 from utils.utils import *
+from frames import intro
 
 root = document["root"]
 content = document["content-container"]
 screen_height = window.innerHeight
 screen_width = window.innerWidth
 
+def change_color():
+    mem_block = document.select('.memory')[0]
+    memory_block.mark_active(mem_block)
+
+def change_color2():
+    mem_block = document.select('.memory')[0]
+    memory_block.mark_used(mem_block)
+
+def set_timeout(ev):
+    timer.set_timeout(change_color, 0)
+
 def say_some_foolery():
-    memblocks = [show() for i in range(10)]
-    
-    for i in range(0, 3):
-        mark_used(memblocks[i])
-        
-    for i in range(3, 7):
-        mark_active(memblocks[i])
+    intro.present(content)
 
 
-    for memblock in memblocks:
-        content.appendChild(memblock)
+    # button = document.createElement('button')
+    # button.appendChild(document.createTextNode('Color changer'))
+    # button.style.alignSelf = "center"
 
-      
+    # chain = TimedEventChain()
+    # chain.add(1000, change_color)
+    # chain.add(1000, change_color2)
+
+    # button.bind('click', chain.start_function)
+
+    # block = memory_block.show()
+    # block.classList.add('memory')
+
+    # content.appendChild(block)
+    # content.appendChild(button)
 
 say_some_foolery()
