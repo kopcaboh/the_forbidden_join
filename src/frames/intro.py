57b7b4ee3b50ae25be41224bf5e608bdba919a1a
kopcaboh <kopcaboh@fel.cvut.cz>
2024-03-25 12:41:06 +0100

    Proper animation for first scene of intro

--- src/frames/intro.py
+++ src/frames/intro.py
@@ -1,7 +1,7 @@
 from browser import document, html, window
 from utils.utils import *
 
-def layout():
+def present(stage):
     scene = document.createElement('div')
     scene.id = 'scene'
     scene.style.justifyContent = 'center'
@@ -10,19 +10,20 @@ def layout():
 
     element = document.createElement('h1')
     element.appendChild(document.createTextNode("Nested loop join"))
+    element.classList.add('fade-in')
+    element.style.animationDelay = "0ms"
     scene.appendChild(element)
     
     element = document.createElement('h2')
     element.appendChild(document.createTextNode("The default (brute force) algorithm"))
+    element.classList.add('fade-in')
+    element.style.animationDelay = "500ms"
     scene.appendChild(element)
 
     element = document.createElement('button')
     element.appendChild(document.createTextNode("Continue >"))
+    element.classList.add('fade-in')
+    element.style.animationDelay = "1000ms"
     scene.appendChild(element)
-
-    return scene
-
-def present(stage):
-    scene = layout()
-
+    
     stage.appendChild(scene)
